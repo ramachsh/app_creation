cf2c1958c68929a402b90cd9385bb69902c4c637
Shivakumar <ramachsh@gmail.com>
2025-08-03 11:21:22 +0530

    Refactor tab layout to simplify header styling and update icons; remove unused components and styles for cleanup

--- app/(tabs)/_layout.tsx
+++ app/(tabs)/_layout.tsx
@@ -1,45 +1,35 @@
-import { Tabs } from 'expo-router';
-import React from 'react';
-import { Platform } from 'react-native';
+import { Ionicons } from "@expo/vector-icons";
+import { Tabs } from "expo-router";
 
-import { HapticTab } from '@/components/HapticTab';
-import { IconSymbol } from '@/components/ui/IconSymbol';
-import TabBarBackground from '@/components/ui/TabBarBackground';
-import { Colors } from '@/constants/Colors';
-import { useColorScheme } from '@/hooks/useColorScheme';
-
-export default function TabLayout() {
-  const colorScheme = useColorScheme();
-
+export default function TabsLayout() {
   return (
     <Tabs
       screenOptions={{
-        tabBarActiveTintColor: Colors[colorScheme ?? 'light'].tint,
+        tabBarActiveTintColor: "#ffd33d",
+        headerStyle: {
+          backgroundColor: "#25292e",
+        },
+        headerShadowVisible: false,
+        headerTintColor: "#fff",
+      }}
+      >
+      <Tabs.Screen name="index" 
+      options={{
+        headerTitle: "Hello Organizer",
+        tabBarIcon: ({focused,color}) => <Ionicons name={focused ? "home" : "home-outline"}
+        size={30} color={color} />,
+      }}
+      />
+      <Tabs.Screen name="about" 
+      options={{
+        headerTitle: "About"
+      }}      
+      />
+      <Tabs.Screen name="not-found" 
+      options={{
         headerShown: false,
-        tabBarButton: HapticTab,
-        tabBarBackground: TabBarBackground,
-        tabBarStyle: Platform.select({
-          ios: {
-            // Use a transparent background on iOS to show the blur effect
-            position: 'absolute',
-          },
-          default: {},
-        }),
-      }}>
-      <Tabs.Screen
-        name="index"
-        options={{
-          title: 'Home',
-          tabBarIcon: ({ color }) => <IconSymbol size={28} name="house.fill" color={color} />,
-        }}
-      />
-      <Tabs.Screen
-        name="explore"
-        options={{
-          title: 'Explore',
-          tabBarIcon: ({ color }) => <IconSymbol size={28} name="paperplane.fill" color={color} />,
-        }}
-      />
+      }}      
+      />      
     </Tabs>
   );
 }
--- app/(tabs)/about.tsx
+++ app/(tabs)/about.tsx
@@ -1,19 +1,13 @@
-import { Link } from "expo-router";
 import { StyleSheet, Text, View } from "react-native";
 
-export default function Index() {
+export default function AboutScreen() {
   return (
     <View
       style={styles.container}>
-      <Text style={styles.text}>Hurray!!My FFirst Mobile App!!
-      </Text>
-      <Link href={"/about"} style={styles.button}>
-      Go to About Screen
-      </Link>
+      <Text style={styles.text}>Thank You For Visiting My Mobile App!!</Text>
     </View>
   );
 }
-
 const styles = StyleSheet.create({
   container: {
     flex: 1,
@@ -22,11 +16,7 @@ const styles = StyleSheet.create({
     backgroundColor: "#7d887eff",
   },
   text: {
-    color: "#fff",
-  },
-  button: {
     fontSize: 20,
-    textDecorationLine: "underline",
     color: "#fff",
-  },
+  }
 })
